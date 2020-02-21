bd1618308034c08e45c0bbbbc692f4dfc75d9ef4
Peter Dragos <pdragos@u.rochester.edu>
2020-02-21 10:27:00 -0500

    removed unused file. Linted and added docstrings.

--- apollo_flask/__init__.py
+++ apollo_flask/__init__.py
@@ -1,7 +1,24 @@
+"""
+This module provides an app factory for apollo_flask.
+"""
+
 import os
 from flask import Flask, render_template, current_app
 
-def create_app(test_config = None):
+def create_app(test_config=None):
+    """
+    This function is an app_factory for apollo_flask.
+
+    Configuration is passed in via one of three ways:
+        - via `instance/config.py` when the code is not installed as a module
+        - via '.../var/apollo_flask-instance/config.py`, if installed as a module
+               via pip (possibly under a virtual environment)
+        - via a dictionary passed in a the test_conifg kwarg.
+
+    Once the configuration is loaded, the database is attached to the application.
+
+    Then, routes and blueprints are added, and an app is returned.
+    """
     #Second argument ensures configuration is loaded from the instance folder
     app = Flask(__name__, instance_relative_config=True)
     if test_config is None:
@@ -13,7 +30,7 @@ def create_app(test_config = None):
 
     #ensure instance folder exists
     os.makedirs(app.instance_path, exist_ok=True)
-    
+
     # Set up database
     import apollo_flask.db as db
     app.config['ENGINE'] = db.create_engine(app.config['DATABASE_URI'])
@@ -22,13 +39,12 @@ def create_app(test_config = None):
     @app.route('/')
     def homepage():
         return render_template('index.html',
-                               project_name = current_app.config['PROJECT_NAME'])
-    
+                               project_name=current_app.config['PROJECT_NAME'])
+
     from . import garmin_oauth
-    app.register_blueprint(garmin_oauth.bp) 
+    app.register_blueprint(garmin_oauth.bp)
 
     from . import garmin_api_client
     app.register_blueprint(garmin_api_client.bp)
 
     return app
-
